c3d03017e22cc68ffcca1df265ed9c6f1e09dc96
LucaPaterlini <LucaPaterlini@github.com>
2018-05-14 17:32:39 +0100

    update version numbers

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
 static const int PROTOCOL_VERSION = 80101;
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 70206;
 
 //! In this version, 'getheaders' was introduced.
 static const int GETHEADERS_VERSION = 70077;
@@ -23,14 +23,14 @@ static const int MIN_PEER_PROTO_VERSION = 80101;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 70206;
 
 //! only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 32000;
 static const int NOBLKS_VERSION_END = 32400;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 80101;
 
 //! "mempool" command, enhanced "getdata" behavior starts with this version
 static const int MEMPOOL_GD_VERSION = 60002;
